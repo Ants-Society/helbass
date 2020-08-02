da9ceb0390c327471f0372d6dbaa2bc07b05741b
baltazarparra <baltazarparra@outlook.com>
2020-08-02 12:31:54 -0300

    feat: add PWA

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
 const button = document.querySelector('.brand__button')
 const contact = document.querySelector('.contact')
 
---
+++ sw.js
@@ -0,0 +1,49 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/style.css',
+  '/img/goodbom.png',
+  '/img/icon.svg',
+  '/img/law-statue.png',
+  '/img/logo.svg',
+  '/img/menu.svg',
+  '/img/mobile-bg.png',
+  '/img/money.svg',
+  '/img/paguemenos.png',
+  '/img/paper.svg',
+  '/img/parana.png',
+  '/img/roberto.png',
+  '/img/search.svg',
+  '/img/terra.png',
+  '/img/vicente.png',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
